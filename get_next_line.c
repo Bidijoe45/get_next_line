6eb9b68b4afa05701cd2b9713fe371f9851ff26e
Adrian nicusor Pavel <apavel@c2r16s5.42madrid.com>
2020-01-30 14:08:32 +0100

    aa

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: apavel <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 12:15:29 by apavel            #+#    #+#             */
-/*   Updated: 2020/01/29 17:41:34 by apavel           ###   ########.fr       */
+/*   Updated: 2020/01/30 14:08:16 by apavel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,67 @@
 
 #include <stdio.h>
 
-
 int get_next_line(int fd, char **line)
 {
-	static char	*rest[4092];
+	static char	*stack[4092];
 	char buff[BUFFER_SIZE + 1];
-	int	b;
 	char *tmp_str;
-
-	if (fd < 0 || !fd || read(fd, buff, 0) < 0)
+	int	b;	
+	
+	if (fd < 0 || read(fd, buff, 0) == -1 || !line)
 		return (-1);
+	
+	/*
+	 *	------
+	 * Hola que
+	 * que tal
+	 *	-------
+	 *	
+	 *	BUFF: Hola que\nque tal
+	 *	STATIC: 
+	 *
+	 *	LINE: Hola que\0
+	 *
+	 *
+	 *
+	 */
+
+	//Comprobar si hay algo en static
+	
+
+	//Leer archivo y guardar hasta /n
 	
 	while ((b = read(fd, buff, BUFFER_SIZE)) > 0)
 	{
 		buff[b] = '\0';
-		//printf("bytes read %d\n", b);
+		//Si existe un salto de linea em buff salimos del bucle
+		//	if (!stack[fd])
+		//		stack[fd] = ft_strdup(buff);
+//			
 		if (ft_get_new_line(buff) == -1)
 		{
-			if (!rest[fd])
-			{
-				rest[fd] = ft_strdup(buff);
-			}
-			else
-			{
-				tmp_str = ft_strdup(rest[fd]);
-				free(rest[fd]);
-				rest[fd] = ft_strjoin(tmp_str, buff);
+//			if (!stack[fd])
+//				stack[fd] = ft_strdup(buff);
+//			else
+//			{
+				tmp_str = ft_strdup(stack[fd]);
+				free(stack[fd]);
+				stack[fd] = ft_strjoin(tmp_str, buff);
 				free(tmp_str);
-			}
+//			}
 		}
 		else
 		{
-			tmp_str = ft_substr(buff, 0, ft_get_new_line(buff));
-			*line = ft_strjoin(rest[fd], tmp_str);
-			free(tmp_str);
-			free(rest[fd]);
-			rest[fd] = ft_substr(buff, ft_get_new_line(buff) + 1, ft_strlen(buff) - ft_get_new_line(buff));
-			if (b < BUFFER_SIZE)
-				return (0);
-			return (1);
+			*line = ft_substr(stack[fd], 0, ft_get_new_line(stack[fd]));
+			free(stack[fd]);
+	//		tmp_str = ft_strdup(&buff[ft_get_new_line(buff)])
+	//		stack[fd] = ft_strdup(tmp_str);
+			break ;
 		}
+	
 	}
 
-	return (0);
+	return (1);
 }
 
 
